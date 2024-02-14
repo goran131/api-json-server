a045de734fddd7b65f5ec620091df3c7f09b21a0
goran131 <goran.rosenberg@tomelilla.nu>
2024-02-14 23:46:04 +0100

    Revert "Added cors"
    This reverts commit beb8b10862e57e866e57adafd81423a066fd5051.

--- api/server.js
+++ api/server.js
@@ -1,24 +1,14 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-const cors = require('cors')
 
 const server = jsonServer.create()
-
-server.use(
-    cors({
-        origin: [
-            'http://localhost:5173',
-            'https://labb-3-skivregister.vercel.app'
-        ]
-    })
-)
 
 // Uncomment to allow write operations
 const fs = require('fs')
 const path = require('path')
 const filePath = path.join('db.json')
-const data = fs.readFileSync(filePath, 'utf-8')
-const db = JSON.parse(data)
+const data = fs.readFileSync(filePath, 'utf-8');
+const db = JSON.parse(data);
 const router = jsonServer.router(db)
 
 // Comment out to allow write operations
@@ -28,12 +18,10 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(
-    jsonServer.rewriter({
-        '/api/*': '/$1',
-        '/blog/:resource/:id/show': '/:resource/:id'
-    })
-)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
